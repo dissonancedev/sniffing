9cf1fcf8643e0eb585ce79bed22b745c1444d608
root <root@echidna-music.org>
2013-08-24 18:44:44 +0000

    First commit

---
+++ sites/all/modules/node_field/theme/tpl/node_field_field.tpl.php
@@ -0,0 +1,6 @@
+<div class="node-field <?php print $class; ?>">
+  <?php if ($show_title) : ?>
+    <label class="title"><?php print $title; ?></label>
+  <?php endif; ?>
+  <div class="body"><?php print $value; ?></div>
+</div>
