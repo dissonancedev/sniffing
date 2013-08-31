771ab59ac0e5f09a17332fc5b894268edc1991f5
ichionid <ichionid@gmail.com>
2013-08-31 15:43:28 +0000

    Removing tags from page.tpl.php

--- sites/all/themes/responsive_bartik/templates/page.tpl.php
+++ sites/all/themes/responsive_bartik/templates/page.tpl.php
@@ -154,11 +154,6 @@
         </h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?>
-        <div class="tabs">
-          <?php print render($tabs); ?>
-        </div>
-      <?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links">
